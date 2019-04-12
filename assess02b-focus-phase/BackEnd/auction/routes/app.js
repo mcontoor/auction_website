ff155d1bd8f390503e9f189fb79ac82de40144ec
mcontoor <mcontoor@gmail.com>
2019-04-12 11:15:58 +0530

    create user registration

--- assess02b-focus-phase/BackEnd/auction/routes/app.js
+++ assess02b-focus-phase/BackEnd/auction/routes/app.js
@@ -1,8 +1,25 @@
 const router = require('express').Router();
+const User = require('../models/users')
+var path = require('path');
 
 router.get('/', (req, res) => {
     // res.sendStatus(200);
     res.send('<h1>Routes running</h1>');
+})
+
+router.get('/register', (req, res)=> {
+    res.sendFile(path.resolve('add-user.html'))
+})
+
+router.post('/register', (req, res) => {
+    var user = new User()
+    user.username = req.body.username,
+    user.password = req.body.password,
+    user.email = req.body.email
+
+    user.save()
+    .then(() => console.log('new user created'), res.send(`${user.username} registered. Login to continue`))
+    .catch(err => console.log(err))
 })
 
 module.exports = router;
--- assess02b-focus-phase/BackEnd/auction/server.js
+++ assess02b-focus-phase/BackEnd/auction/server.js
@@ -1,12 +1,20 @@
 const express = require('express')
 const mongoose = require('mongoose');
 const Routes = require('./routes/app')
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect('mongodb://localhost:27017/auction', {useNewUrlParser : true})
+mongoose.connect('mongodb://localhost:27017/auction', {
+    useNewUrlParser : true,
+    useCreateIndex: true
+})
 .then(() => console.log('database connected'))
 .catch(err => console.log(err))
 
