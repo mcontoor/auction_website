64b35d45228bec77d76972d2680aed135a78752b
Your Name <you@example.com>
2019-04-13 00:30:32 +0530

    passport

---
+++ submit/app.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const mongoose =require('mongoose');  
+const flash = require('connect-flash');
+const session = require('express-session');
+const passport = require('passport');
+const app = express();
+
+require('./config/passport')(passport);
+
+const db = require('./config/index').url;
+
+mongoose.connect(db, { useNewUrlParser: true })
+.then(() => console.log('MongoDB connected..'))
+.catch(err => console.log(err));
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({ extended: false}));
+
+app.use(session({
+    secret:'secret',
+    resave: false,
+    saveUninitialized: true,
+    
+}))
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(flash());
+
+app.use((req,res,next)=> {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error.msg');
+    res.locals.error = req.flash('error');
+    next();
+})
+
+app.use('/', require('./routes/index'));
+app.use('/users', require('./routes/users'));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, console.log(`server started on port ${PORT}`));
