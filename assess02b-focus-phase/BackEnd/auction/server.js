054c99fe823022a1d4e8c1fe9adfd85bdcd8a1d0
mcontoor <mcontoor@gmail.com>
2019-04-12 07:45:15 +0530

    make db connection

--- assess02b-focus-phase/BackEnd/auction/server.js
+++ assess02b-focus-phase/BackEnd/auction/server.js
@@ -1,8 +1,13 @@
 const express = require('express')
+const mongoose = require('mongoose');
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+
+mongoose.connect('mongodb://localhost:27017/auction', {useNewUrlParser : true})
+.then(() => console.log('database connected'))
+.catch(err => console.log(err))
 
 app.use('/', (req,res) =>
     res.send('Server running')
